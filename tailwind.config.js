7a7c66112c7cf0ae2d372794c700556297d6ede2
AndresMiami <Andrescorcoba5@gmail.com>
2025-05-14 18:05:13 -0500

    Enhanced input field and heading styling for luxury aesthetic

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",  // Scan HTML files in the root
-    "./src/**/*.{js,ts,jsx,tsx,html}", // Scan relevant files in src folder and subfolders
-    // Add any other paths where you use Tailwind classes
+    "./*.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-teal': '#00A09A',
+        'color-teal-dark': '#008080',
+        'color-gold': '#B08D57',
+        'color-gold-dark': '#A07844',
+        'color-palm-green': '#3CB371',
+        'dark-brown': 'rgb(28 22 4 / 90%)', // Custom background color
+      },
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+      },
+      padding: {
+        '0.2': '0.05rem',
+      },
+      maxWidth: {
+        'custom': '50rem', // 800px
+        'narrow-sm': '25rem', // 400px (optional)
+        'narrow-xl': '37.5rem', // 600px (optional)
+      },
+      height: {
+        'custom': '37.5rem', // 600px
+      },
+    },
   },
   plugins: [],
 }
